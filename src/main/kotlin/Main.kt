ce76fd43b0a51e0d9327105116ef5844ce4bc886
Kevin <kevin.vandenbreemen@gmail.com>
2025-07-18 12:40:30 -0700

    Trying to get a basic compose app up and running

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,37 @@
+package com.vandenbreemen
+
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+////TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+//// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//fun main() {
+//    val name = "Kotlin"
+//    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+//    // to see how IntelliJ IDEA suggests fixing it.
+//    println("Hello, " + name + "!")
+//
+//    for (i in 1..5) {
+//        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+//        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+//        println("i = $i")
+//    }
+//}
+
+
+@Composable
+@Preview
+fun App() {
+
+    MaterialTheme {
+
+        Column {
+            Text("Hello world!")
+        }
+
+    }
+
+}
