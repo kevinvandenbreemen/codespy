08e01b6da76dc9296c7893272a22f63e9a658be7
Kevin <kevin.vandenbreemen@gmail.com>
2025-07-18 15:44:31 -0700

    Assemble the model / cache the types when possible

--- src/main/kotlin/com/vandenbreemen/codespy/interactor/GrucdInteractor.kt
+++ src/main/kotlin/com/vandenbreemen/codespy/interactor/GrucdInteractor.kt
@@ -1,18 +1,27 @@
 package com.vandenbreemen.com.vandenbreemen.codespy.interactor
 
 import com.vandenbreemen.grucd.builder.SourceCodeExtractor
+import com.vandenbreemen.grucd.model.Model
 import java.io.File
 
 class GrucdInteractor {
 
-    //  TODO Need Cache of source code extractors for the past 3 requests
-
+    private var extractor: SourceCodeExtractor? = null
 
     fun getSourceCodeFiles(path: File): List<String>  {
 
         val extractor = SourceCodeExtractor().detectFileDeltas()
-        return extractor.getFilenamesToVisit(null, path.absolutePath)
+        return extractor.getFilenamesToVisit(null, path.absolutePath).also {
+            if(it.isNotEmpty()) {
+                this.extractor = extractor
+            }
+        }
 
+    }
+
+    fun getModel(path: File): Model {
+        return extractor?.updateModelWithFileChanges(path.absolutePath)
+            ?: throw IllegalStateException("Extractor is not initialized. Call getSourceCodeFiles() first.")
     }
 
 }
--- src/main/kotlin/com/vandenbreemen/codespy/viewmodel/CodeSpyViewModel.kt
+++ src/main/kotlin/com/vandenbreemen/codespy/viewmodel/CodeSpyViewModel.kt
@@ -3,6 +3,7 @@ package com.vandenbreemen.com.vandenbreemen.codespy.viewmodel
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.State
 import com.vandenbreemen.com.vandenbreemen.codespy.interactor.GrucdInteractor
+import com.vandenbreemen.grucd.model.Model
 import java.io.File
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -13,14 +14,27 @@ class CodeSpyViewModel(private val grucdInteractor: GrucdInteractor) {
     val directoryMessage: State<String> = _directoryMessage
     private val viewModelScope = CoroutineScope(Dispatchers.IO)
 
+    private var currentDirectory: File? = null
+
+    private var model: Model? = null
+
     fun selectNewDirectory(path: File) {
         viewModelScope.launch {
             val files = grucdInteractor.getSourceCodeFiles(path)
             _directoryMessage.value = if (files.isNotEmpty()) {
-                "Found ${files.size} parsable file(s) in the selected directory."
+                "Software System in directory: ${path.absolutePath}"
             } else {
                 "No parsable files found in the selected directory."
             }
+
+            if(files.isNotEmpty()) {    //  Only update current directory if we found files
+                currentDirectory = path
+
+                //  Now build model
+                model = grucdInteractor.getModel(path)
+            }
+
+
         }
     }
 
