2cd6837453f30db81dfdffa2c2e8d8c014caae1a
Kevin <kevin.vandenbreemen@gmail.com>
2025-07-18 13:20:11 -0700

    Get the basic app to show

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,35 +3,69 @@ package com.vandenbreemen
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.IconButton
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.DrawerValue
+import androidx.compose.material.rememberDrawerState
+import androidx.compose.material.ModalDrawer
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
+import kotlinx.coroutines.launch
 
-////TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-//// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-//fun main() {
-//    val name = "Kotlin"
-//    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-//    // to see how IntelliJ IDEA suggests fixing it.
-//    println("Hello, " + name + "!")
-//
-//    for (i in 1..5) {
-//        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-//        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-//        println("i = $i")
-//    }
-//}
 
 
 @Composable
 @Preview
 fun App() {
+    val drawerState = rememberDrawerState(DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
 
     MaterialTheme {
-
-        Column {
-            Text("Hello world!")
+        ModalDrawer(
+            drawerState = drawerState,
+            drawerContent = {
+                Column {
+                    Text("Menu Item 1")
+                    Text("Menu Item 2")
+                }
+            }
+        ) {
+            Scaffold(
+                topBar = {
+                    TopAppBar(
+                        title = { Text("My App Title") },
+                        navigationIcon = {
+                            IconButton(onClick = { scope.launch { drawerState.open() } }) {
+                                androidx.compose.material.Icon(Icons.Filled.Menu, contentDescription = "Menu")
+                            }
+                        }
+                    )
+                }
+            ) {
+                Column {
+                    Text("Hello world!")
+                }
+            }
         }
 
     }
 
+}
+
+fun main() = application {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "CodeSpy",
+        state = rememberWindowState(width = 1000.dp, height = 800.dp)
+    ) {
+        App()
+    }
 }
