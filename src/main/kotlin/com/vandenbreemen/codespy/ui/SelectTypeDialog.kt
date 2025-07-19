bc4ffbfde23711ac94ab0705e18670785108f218
Kevin <kevin.vandenbreemen@gmail.com>
2025-07-18 22:27:27 -0700

    Make this thing scrollable

--- src/main/kotlin/com/vandenbreemen/codespy/ui/SelectTypeDialog.kt
+++ src/main/kotlin/com/vandenbreemen/codespy/ui/SelectTypeDialog.kt
@@ -1,10 +1,14 @@
 package com.vandenbreemen.codespy.ui
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material.AlertDialog
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
@@ -30,13 +34,23 @@ fun SelectTypeDialog(
         title = { Text("Select Type to View") },
         text = {
             Column {
+
+                val verticalScrollState = rememberScrollState()
+
                 OutlinedTextField(
                     value = query,
                     onValueChange = { query = it },
                     label = { Text("Type name") },
                     modifier = Modifier.fillMaxWidth()
                 )
-                LazyColumn(modifier = Modifier.fillMaxWidth().padding(top = 8.dp)) {
+                LazyColumn(
+                    modifier = Modifier.fillMaxWidth().padding(top = 8.dp)
+                        .height(250.dp)
+                        .scrollable(
+                            state = verticalScrollState,
+                            orientation = Orientation.Vertical
+                        )
+                ) {
                     items(filteredTypes.size) { idx ->
                         val type = filteredTypes[idx]
                         val displayText = "${type.pkg}.${type.name}"
